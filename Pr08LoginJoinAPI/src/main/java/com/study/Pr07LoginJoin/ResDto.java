22ad0731e88d87f1b1e33108755ebbbd707109e3
jyoon0531 <chupki0531@gmail.com>
2024-04-04 17:42:34 +0900

    pr08 end

--- Pr08LoginJoinAPI/src/main/java/com/study/Pr07LoginJoin/ResDto.java
+++ Pr08LoginJoinAPI/src/main/java/com/study/Pr07LoginJoin/ResDto.java
@@ -1,8 +1,11 @@
 package com.study.Pr07LoginJoin;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class ResDto {
+    private String status;
     private String message;
 }
